15c60d8586c20f98f8ec9f9ed4030da7367e2a56
Cheng Peng <pengchengkevin@hotmail.com>
2020-09-29 23:04:12 -0700

    added auth

---
+++ functions/dbschema.js
@@ -0,0 +1,12 @@
+//format of data, no practice use
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream bosy",
+      createdAt: "2020-09-30T11:55:43.162Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const express = require("express");
+const app = require("express")();
+const firebase = require("firebase");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA6bVMUwVZ6XjD3r-c6H-bQ98v7XOT1P_w",
+  authDomain: "socialape-454be.firebaseapp.com",
+  databaseURL: "https://socialape-454be.firebaseio.com",
+  projectId: "socialape-454be",
+  storageBucket: "socialape-454be.appspot.com",
+  messagingSenderId: "591094817889",
+  appId: "1:591094817889:web:99063391dd0f2b4fa37620",
+  measurementId: "G-GPQR6J424E",
+};
 
 admin.initializeApp();
-const app = express();
+firebase.initializeApp(firebaseConfig);
 
 app.get("/screams", (req, res) => {
   admin
@@ -30,7 +42,7 @@ app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
   };
   admin
     .firestore()
@@ -42,6 +54,31 @@ app.post("/scream", (req, res) => {
     .catch((err) => {
       res.status(400).json({ error: "something went wrong" });
       console.error(err);
+    });
+});
+
+//signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // TODO: validate data
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return res
+        .status(201)
+        .json({ message: `user ${data.user.uid} signedup successfully ` });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
     });
 });
 
