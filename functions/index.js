fdd19f15b5aefa8a6e27cda0d3712f9ba6897091
Cheng Peng <pengchengkevin@hotmail.com>
2020-09-29 18:05:36 -0700

    First commit

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+
+admin.initializeApp();
+const app = express();
+
+app.get("/screams", (req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST")
+    return res.status(400).json({ error: "Method not allowed" });
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
