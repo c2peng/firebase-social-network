e7713825a5f00ba2f5dc9f27566a17aea8567582
Cheng Peng <pengchengkevin@hotmail.com>
2020-10-03 20:19:32 -0700

    Refactor

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyA6bVMUwVZ6XjD3r-c6H-bQ98v7XOT1P_w",
+  authDomain: "socialape-454be.firebaseapp.com",
+  databaseURL: "https://socialape-454be.firebaseio.com",
+  projectId: "socialape-454be",
+  storageBucket: "socialape-454be.appspot.com",
+  messagingSenderId: "591094817889",
+  appId: "1:591094817889:web:99063391dd0f2b4fa37620",
+  measurementId: "G-GPQR6J424E",
+};
