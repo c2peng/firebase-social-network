cfce93a742521621ce178980dd257f1b8ef5adc3
Cheng Peng <pengchengkevin@hotmail.com>
2020-10-10 23:36:48 -0700

    notification system

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const app = require("express")();
+const { db } = require("./util/admin");
 
 const {
   getAllScreams,
@@ -27,8 +28,6 @@ app.delete("/scream/:screamId", FBAuth, deleteScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 app.post("/scream/:screamId/like", FBAuth, likeScream);
 app.post("/scream/:screamId/unlike", FBAuth, unlikeScream);
-// TODO: unliking a scream
-// TODO: comment on scream
 
 app.post("/signup", signup);
 app.post("/login", login);
@@ -37,3 +36,73 @@ app.post("/user/", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
+exports.createNotificationOnLike = functions
+  .region("us-east1")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.err(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnlike = functions
+  .region("us-east1")
+  .firestore.document("/likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.err(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region("us-east1")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    console.log("id:" + snapshot.data().screamId);
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
